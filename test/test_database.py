b88e12580fc3410b70b992e49c42514a952d18d1
Bill Leddy <bill@williesworkshop.net>
2018-11-06 15:21:04 -0800

    create database tests

--- test/test_database.py
+++ test/test_database.py
@@ -1,22 +1,40 @@
 import sys
-sys.path.append('') ##get import to look in the working dir.
+sys.path.append('') ##get import to look in the working dir
+
+import app
+import sqlite3
+import takeabeltof.database as dbm
 
 filespec = 'instance/test_database.db'
-
 db = None
+
+with app.app.app_context():
+    db = app.get_db(filespec)
+    app.init_db(db)
 
 def delete_test_db():
         os.remove(filespec)
 
 def test_database():
-    from takeabeltof.database import Database
-    db2 = Database(filespec)
-    assert type(db2) is Database
+    assert type(db) is sqlite3.Connection
+    assert db.row_factory == sqlite3.Row
+    rec = db.execute('PRAGMA foreign_keys').fetchone()
+    assert rec[0] == 1 # foreign key support is on
+    
+def test_database_cursor():
+    cursor = db.cursor()
+    assert type(cursor) == sqlite3.Cursor
+    
+    
+### Should test the SqliteTable class here, but too tired now...
+    
+    
 
 ############################ The final 'test' ########################
 ######################################################################
 def test_finished():
     try:
+        db.close()
         delete_test_db()
         assert True
     except:
