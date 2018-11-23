b7fbfa5a43301a0e5e56dccd6601501395a98e86
Bill Leddy <bill@williesworkshop.net>
2018-11-23 13:28:01 -0500

    provide app_context for markdown utils

--- test/test_utils.py
+++ test/test_utils.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append('') ##get import to look in the working dir.
 
 import pytest
-
+from app import app
 import takeabeltof.utils as utils
 
 def test_cleanRecordID():
@@ -29,27 +29,30 @@ def test_looksLikeEmailAddress():
     
     
 def test_printException():
-    # printException((mes="An Unknown Error Occured",level="error",err=None))
-    assert utils.printException(mes="Not an error") == "Not an error"
-    # create an acutal error
-    with pytest.raises(Exception):
-        try:
-            if nothing == True:
-                pass
-        except Exception as e:
-            mes =  utils.printException(mes="Should Be error",err=e)
-            assert "NameError" in mes
+    with app.app_context():
+        # printException((mes="An Unknown Error Occured",level="error",err=None))
+        assert utils.printException(mes="Not an error") == "Not an error"
+        # create an acutal error
+        with pytest.raises(Exception):
+            try:
+                if nothing == True:
+                    pass
+            except Exception as e:
+                mes =  utils.printException(mes="Should Be error",err=e)
+                assert "NameError" in mes
             
 def test_render_markdown_for():
-    # render_markdown_for(source_script,module,file_name):   
-    from flask import Blueprint
-    mod = Blueprint('testme',__name__, template_folder='/takeabeltof/test/templates') 
-    result = utils.render_markdown_for(__name__,mod,'test_script.md')
-    assert "no file found" in result
-    result = utils.render_markdown_for(__name__,mod,'takeabeltof.md')
-    assert "no file found" not in result
-    assert "<h1>Takeabeltof</h1>" in result
+    with app.app_context():
+        # render_markdown_for(source_script,module,file_name):   
+        from flask import Blueprint
+        mod = Blueprint('testme',__name__, template_folder='/takeabeltof/test/templates') 
+        result = utils.render_markdown_for(__name__,mod,'test_script.md')
+        assert "no file found" in result
+        result = utils.render_markdown_for(__name__,mod,'takeabeltof.md')
+        assert "no file found" not in result
+        assert "<h1>Takeabeltof</h1>" in result
     
 def test_render_markup_text():
-    result = utils.render_markdown_text("## Now is the time")
-    assert "<h2>Now is the time</h2>" in result
+    with app.app_context():
+        result = utils.render_markdown_text("## Now is the time")
+        assert "<h2>Now is the time</h2>" in result
